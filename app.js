2db0f0e5f8464ee43386955b1ee38d7b23809d2d
Koidu Kannela <koidkan@greeny.cs.tlu.ee>
2016-03-07 16:41:03 +0200

    error

--- app.js
+++ app.js
@@ -1,42 +1,17 @@
 (function(){
   "use strict"; //aitab vältida globaalseid muutujaid
 
+
   // SINGLETON PATTERN (4 rida) - üksik objekt, mis keelab objekti mitmekordse välja kutsumise.
   var Calendar = function(){
     if(Calendar.instance){
       return Calendar.instance;
     }
     Calendar.instance = this;
-    this.routes = Calendar.routes;
-    console.log(this); //writing into the browser console
 
-    this.currentRoute = null;
-    this.interval = null;
+
     //siin paneme rakenduse tööle
     this.init();
-  };
-
-
-
-  //iga lehe kohta hoiame meeles, mis lehel oleme
-  Calendar.routes = {
-    "home-view":{
-      render: function(){
-        console.log('Home page');
-      }
-    },
-
-    "calendar-view":{
-      render: function(){
-        console.log('Calendar page');
-      }
-    },
-
-    "to-do-view":{
-      render: function(){
-        console.log('To do page');
-      }
-    }
   };
 
 
@@ -47,48 +22,63 @@
   Calendar.prototype = {
     init: function(){
       console.log('rakendus käivitus');
-      window.addEventListener('hashchange', this.routeChange.bind(this));
-      //vaatan, mis lehel olen
-      console.log(window.location.hash);
-        if(!window.location.hash){
-          window.location.hash = "#home-view";
-        }else{
-          this.routeChange();
-        }
       this.bindMouseEvents();
     },
 
     bindMouseEvents: function(){
-      document.querySelector('add-new-meetings').addEventListener('click', this.addNewClic.bind(this));
+      document.querySelector('.add-new-meetings').addEventListener('click', this.addNewClic.bind(this));
     },
 
     addNewClic: function(event){
-      this_click_count++;
-      console.log(this_click_count);
-    },
+      var start_date = document.querySelector('.start-date').value;
+      var start_time = document.querySelector('.start-time').value;
+      var finish_date = document.querySelector('.finish-date').value;
+      var finish_time = document.querySelector('.finish-time').value;
+      var meeting_description = document.querySelector('.meeting-description').value;
+      //console.log(start_date+' '+start_time+' '+finish_date+' '+finish_time+' '+meeting_description);
 
-
-    routeChange: function(event){
-      this.currentRoute = window.location.hash.slice(1);
-      //kas leht on olemas
-      if(this.routes[this.currentRoute]){
-        this.updateMenu();
-        console.log('>>>' + this.currentRoute);
-        this.routes[this.currentRoute].render();
-      }else{
-        console.log('404');
-        window.location.hash = "#home-view";
-      }
-    },
-
-    updateMenu: function(){
-      // kui on menuul klass active menu, siis v6tame 2ra
-      document.querySelector('.active-menu').className = document.querySelector('.active-menu').className.replace(' active-menu', '');
-
-      document.querySelector('.'+this.currentRoute).className += ' active-menu';
-      //lisan juurde
+      var new_meeting = new Meeting(start_date, start_time, finish_date, finish_time, meeting_description);
+      var li = new_meeting.createHtmlElement();
+      document.querySelector('.list-of-meetings').appendChild(li);
     }
   };
+
+  var Meeting = function(new_start_date, new_start_time, new_finish_date, new_finish_time, new_meeting_description){
+    this.start_date = new_start_date;
+    this.start_time = new_start_time;
+    this.finish_date = new_finish_date;
+    this.finish_time = new_finish_time;
+    this.meeting_description = new_meeting_description;
+  };
+
+  Meeting.prototype={
+    createHtmlElement: function(){
+      var li = document.createElement('li');
+
+      var span = document.createElement('span');
+      span.className = 'letter';
+
+      var letter = document.createTextNode(this.title.charAt(0));
+      span.appendChild(letter);
+
+      li.appendChild(span);
+
+      var content_span = document.createElement('span');
+      content_span.className = 'content';
+
+      var content = document.createTextNode(this.start_date +'  ' + this.start_time + ' | ' + this.finish_date +'  ' + this.finish_time +'<br>'+ this.meeting_description);
+
+      content_span.appendChild(content);
+
+      li.appendChild(content_span);
+
+      console.log(li);
+
+      return li;
+    }
+  };
+
+
 
 
 
