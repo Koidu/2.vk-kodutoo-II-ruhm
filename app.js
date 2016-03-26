15bdaff06bce33750b33ae51220dff7fa267caa5
Koidu Kannela <koidkan@greeny.cs.tlu.ee>
2016-03-26 16:49:06 +0200

    list

--- app.js
+++ app.js
@@ -9,6 +9,7 @@
     }
     Calendar.instance = this;
 
+    this.collection = [];
 
     //siin paneme rakenduse tööle
     this.init();
@@ -27,6 +28,7 @@
 
     bindMouseEvents: function(){
       document.querySelector('.add-new-meetings').addEventListener('click', this.addNewClic.bind(this));
+      document.querySelector('.add-new-to-do').addEventListener('click', this.addNewClic.bind(this));
     },
 
     addNewClic: function(event){
@@ -34,12 +36,44 @@
       var start_time = document.querySelector('.start-time').value;
       var finish_date = document.querySelector('.finish-date').value;
       var finish_time = document.querySelector('.finish-time').value;
+      var finish_date_task = document.querySelector('.finish-date').value;
+      var finish_time_task = document.querySelector('.finish-time').value;
+      console.log(document.querySelector('.task-description'));
+      console.log(document.querySelector('.meeting-description'));
       var meeting_description = document.querySelector('.meeting-description').value;
       //console.log(start_date+' '+start_time+' '+finish_date+' '+finish_time+' '+meeting_description);
+      var task_description = document.querySelector('.task-description').value;
 
       var new_meeting = new Meeting(start_date, start_time, finish_date, finish_time, meeting_description);
-      var li = new_meeting.createHtmlElement();
-      document.querySelector('.list-of-meetings').appendChild(li);
+      var new_task = new Task(finish_date_task, finish_time_task, task_description);
+
+      this.collection.push(new_meeting);
+      this.collection.push(new_task);
+      console.log(this.collection);
+
+      //sorteeri
+      //this.collection
+      this.collection = this.collection.sort(function(meeting1,meeting2) {
+          return meeting1.start_date > meeting2.start_date;
+       });
+
+       //ksututab k]ik tühjaks
+      document.querySelector('.list-of-meetings').innerHTML = '';
+
+      for(var i = 0; i < this.collection.length; i++){
+        if(this.collection[i].type == "meeting"){
+          console.log('meeting');
+        }
+        var li = this.collection[i].createHtmlElement();
+        document.querySelector('.list-of-meetings').appendChild(li);
+      }
+
+      //LISAD JUURDE
+     //var li = new_meeting.createHtmlElement();
+     //document.querySelector('.list-of-meetings').appendChild(li);
+
+
+
     }
   };
 
@@ -49,6 +83,41 @@
     this.finish_date = new_finish_date;
     this.finish_time = new_finish_time;
     this.meeting_description = new_meeting_description;
+    this.type = "meeting";
+  };
+
+  var Task = function(new_finish_date_task, new_finish_time_task, new_task_description){
+    this.finish_date_task = new_finish_date_task;
+    this.finish_time_task = new_finish_time_task;
+    this.task_description = new_task_description;
+    this.type = "task";
+  };
+
+  Task.prototype={
+    createHtmlElement: function(){
+      var li = document.createElement('li');
+
+      var span = document.createElement('span');
+      span.className = 'letter';
+
+      var letter = document.createTextNode(this.task_description.charAt(0));
+      span.appendChild(letter);
+
+      li.appendChild(span);
+
+      var content_span = document.createElement('span');
+      content_span.className = 'content';
+
+      var content = document.createTextNode(this.finish_date_task +'  ' + this.finish_time_task + ' | ' + this.task_description);
+
+      content_span.appendChild(content);
+
+      li.appendChild(content_span);
+
+      console.log(li);
+
+      return li;
+    }
   };
 
   Meeting.prototype={
@@ -58,7 +127,7 @@
       var span = document.createElement('span');
       span.className = 'letter';
 
-      var letter = document.createTextNode(this.title.charAt(0));
+      var letter = document.createTextNode(this.meeting_description.charAt(0));
       span.appendChild(letter);
 
       li.appendChild(span);
@@ -66,7 +135,7 @@
       var content_span = document.createElement('span');
       content_span.className = 'content';
 
-      var content = document.createTextNode(this.start_date +'  ' + this.start_time + ' | ' + this.finish_date +'  ' + this.finish_time +'<br>'+ this.meeting_description);
+      var content = document.createTextNode(this.start_date +'  ' + this.start_time + ' | ' + this.finish_date +'  ' + this.finish_time +' | '+ this.meeting_description);
 
       content_span.appendChild(content);
 
